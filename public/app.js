f08f026b75a54dd9a972c0f16d38c9cc14284c21
Ilmo Ott <30363925+ilmoott@users.noreply.github.com>
2018-10-03 09:15:47 +1000

    menu update

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const DOMStrings = {
     menu : ".navigation__checkbox",
     menu_link: ".navigation__link",
+    menu_list: ".navigation__list",
     popup_close: ".popup__close",
     popup_book: ".book_close",
     popup: ".popup",
@@ -8,12 +9,18 @@ const DOMStrings = {
 };
 
 const menu = document.querySelector(DOMStrings.menu);
-const menu_list = document.querySelectorAll(DOMStrings.menu_link);
+const menu_link = document.querySelectorAll(DOMStrings.menu_link);
+const menu_list = document.querySelectorAll(DOMStrings.menu_list);
 
-menu_list.forEach(function(e){
+menu_link.forEach(function(e){
     e.addEventListener('click',function(){
         menu.checked = false;
-    })
+    });
+    // if(menu.checked === false){
+    //     menu_list.style.display = "none";
+    // }else{
+    //     menu_list.style.display = "block";
+    // }
 });
 
 
@@ -29,15 +36,15 @@ popup.addEventListener('click',function(e){
 
 
 //NAVIGATION BUTTON
-const navigation = document.querySelector(DOMStrings.navigation);
+// const navigation = document.querySelector(DOMStrings.navigation);
 
-navigation.addEventListener('click', function(){
-    if(navigation.style.display === "none"){
-        navigation.style.display = "block";
-    }else{
-        navigation.style.display = "none";
-    }
-});
+// navigation.addEventListener('click', function(){
+//     if(navigation.style.display === "none"){
+//         navigation.style.display = "block";
+//     }else{
+//         navigation.style.display = "none";
+//     }
+// });
 
 
 
