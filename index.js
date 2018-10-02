469852167bb0cea46f9c67c26b6e39062043ef23
Ilmo Ott <30363925+ilmoott@users.noreply.github.com>
2018-10-02 18:19:29 +1000

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 5000;
 var app = express();
 
 mongoose.connect('mongodb://admin:admin123@ds113923.mlab.com:13923/node_todo_sample', {useNewUrlParser:true}, function(err) {
@@ -49,6 +49,17 @@ app.post('/', function(req, res){
     }
 });
 
-app.listen(port, function(){
-    console.log('Server started');
+// app.listen(port, function(){
+//     console.log('Server started');
+// });
+
+console.log("Calling app.listen().");
+
+var server = app.listen(port, function (){
+  console.log("Calling app.listen's callback function.");
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
+
+console.log("app.listen() executed.");
